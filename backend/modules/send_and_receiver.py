3f4d37cce133e1783479b019dc0143739b8b8954
y0329 <kida_hirokazu@tuta.io>
2023-09-06 13:15:45 +0900

    [Fix]リストの形式をフロントエンドに合わせて修正

--- backend/modules/send_and_receiver.py
+++ backend/modules/send_and_receiver.py
@@ -2,7 +2,7 @@
 from flask import Flask, Response , request
 from flask_cors import cross_origin,CORS
 import time
-
+import json
 # メッセージ管理クラス
 class sse_message():
     def __init__(self):
@@ -14,19 +14,29 @@ class sse_message():
 #タスク管理クラス
 class file_manager():
     def __init__(self):
-        self.tasklist = [{
-            "projectName": "プロジェクトA",
-            "ipAddr": "192.168.0.201",
-            "fileSize": "10GB",
-            "fileName": "test",
-            "progress": 0,
-            "status": "NOT",
-            "icon": "/userIcons/user01.png"
-        }]#下の例をそのまま挿入
+        self.tasklist = [
+  {
+    "projectName": "プロジェクトA",
+    "ipAddr": "192.168.0.201",
+    "fileSize": "10GB",
+    "fileName": "【カット処理済み】〇〇プロジェクト.mp4",
+    "progress": 50,
+    "status": "IN_PROGRESS",
+    "icon": "/userIcons/user01.png"
+  },
+  {
+    "projectName": "プロジェクトB",
+    "ipAddr": "192.168.0.201",
+    "fileSize": "10GB",
+    "fileName": "【カット処理済み】〇〇プロジェクト.mp4",
+    "progress": 50,
+    "status": "IN_PROGRESS",
+    "icon": "/userIcons/user01.png"
+  }]#下の例をそのまま挿入
     def add_task(self,info):#送信予定を送信
         self.tasklist.append(info)
     def get_task(self):
-        return self.tasklist
+        return json.dumps(self.tasklist)
     def change_state(self,name,state):#該当するタスクのstatus変更　中断モジュールやfile_observerからの正常アクセスを意図
         for i in self.tasklist:
             if i["fileName"] == name:
