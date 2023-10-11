48aface868c150723f0f60ee29f851806808240b
y0329 <kida_hirokazu@tuta.io>
2023-10-11 16:37:42 +0900

    [Add]マルチスレッドテスト

--- backend/main.py
+++ backend/main.py
@@ -5,34 +5,15 @@ import json
 import time
 if __name__ == "__main__":
     # Flaskアプリケーションを起動するスレッド
-    app_thread = threading.Thread(target=app.run, kwargs={"host":'0.0.0.0',"port": 5000})
+    app_thread = threading.Thread(target=app.run, kwargs={
+                                  "host": '0.0.0.0', "port": 5000})
     app_thread.start()
 
     # ファイル監視を起動するスレッド
-    file_thread = threading.Thread(target=start_monitoring, args=("/upload",))
+    file_thread = threading.Thread(target=start_monitoring, args=("./upload",))
     file_thread.start()
+    file_thread2 = threading.Thread(target=start_monitoring, args=("./upload2",))
+    file_thread2.start()
+    file_thread3 = threading.Thread(target=start_monitoring, args=("./upload3",))
+    file_thread3.start()
 
-    lists = [
-        {
-            "projectName": "プロジェクトA",
-            "ipAddr": "192.168.0.201",
-            "fileSize": "10GB",
-            "fileName": "【カット処理済み】〇〇プロジェクト.mp4",
-            "progress": 0,
-            "status": "IN_PROGRESS",
-            "icon": "/userIcons/user01.png"
-        },
-    ]
-
-    # 1秒ごとにメッセージを更新する
-    """while True:
-        time.sleep(1)
-        for i in range(1, 100):
-            lists[0]["progress"] = i
-            res = json.dumps(lists)
-            msg.send(res)
-            time.sleep(0.5)
-        
-        lists[0]["status"] = "COMPLETED"
-        res = json.dumps(lists)
-        msg.send(res)"""
--- backend/modules/send_and_receiver.py
+++ backend/modules/send_and_receiver.py
@@ -1,77 +1,79 @@
 # モジュール定義
-from flask import Flask, Response , request
-from flask_cors import cross_origin,CORS
+from flask import Flask, Response, request
+from flask_cors import cross_origin, CORS
 import time
 import json
 # メッセージ管理クラス
+
+
 class sse_message():
     def __init__(self):
         self.message = ""
+
     def read(self):
         return self.message
-    def send(self,value):
+
+    def send(self, value):
         self.message = value
-#タスク管理クラス
+# タスク管理クラス
+
+
 class file_manager():
+    tasklist = [
+            {
+                "projectName": "red",
+                "fileName": "test.txt",
+                "fileSize": "1234567890B",
+                "uniqeId": "af5d656a-31be-4ec8-9039-56038b72c987",
+                "progress": 0,
+                "status": "CREATED"
+            }]
     def __init__(self):
         self.tasklist = [
-  {
-    "projectName": "プロジェクトA",
-    "ipAddr": "192.168.0.201",
-    "fileSize": "10GB",
-    "fileName": "【カット処理済み】〇〇プロジェクト.mp4",
-    "progress": 50,
-    "status": "IN_PROGRESS",
-    "icon": "/userIcons/user01.png"
-  },
-  {
-    "projectName": "プロジェクトB",
-    "ipAddr": "192.168.0.201",
-    "fileSize": "10GB",
-    "fileName": "【カット処理済み】〇〇プロジェクト.mp4",
-    "progress": 50,
-    "status": "IN_PROGRESS",
-    "icon": "/userIcons/user01.png"
-  }]#下の例をそのまま挿入
-    def add_task(self,info):#送信予定を送信
+            {
+                "projectName": "red",
+                "fileName": "test.txt",
+                "fileSize": "1234567890B",
+                "uniqeId": "af5d656a-31be-4ec8-9039-56038b72c987",
+                "progress": 0,
+                "status": "CREATED"
+            }]  # 下の例をそのまま挿入
+
+    def add_task(self, info):  # 送信予定を送信
         self.tasklist.append(info)
+
     def get_task(self):
         return json.dumps(self.tasklist)
-    def change_state(self,name,state):#該当するタスクのstatus変更　中断モジュールやfile_observerからの正常アクセスを意図
+
+    # 該当するタスクのstatus変更　中断モジュールやfile_observerからの正常アクセスを意図
+    def change_state(self, name, state):
         for i in self.tasklist:
             if i["fileName"] == name:
                 i["status"] = state
-    def send_error(self,name):#該当するタスクのstatus変更　一定時間を超えてファイルサイズが変更されていない際やハートビートの失敗などでの変更を意図
+
+    def send_error(self, name):  # 該当するタスクのstatus変更　一定時間を超えてファイルサイズが変更されていない際やハートビートの失敗などでの変更を意図
         for i in self.tasklist:
             if i["fileName"] == name:
                 i["status"] = "error"
-    def delete_task(self,name):#change_stateの完了の表示後やsend_errorのエラー表示後の一定時間後に実行
+
+    def delete_task(self, name):  # change_stateの完了の表示後やsend_errorのエラー表示後の一定時間後に実行
         for i in self.tasklist:
             if i["fileName"] == name:
-                del(i)        
-                
-"""
-送信されるリストの形式　アイコンについては除外してもよいかも
-lists = {
-            "projectName": "プロジェクトA",
-            "ipAddr": "192.168.0.201",
-            "fileSize": "10GB",
-            "fileName": "【カット処理済み】〇〇プロジェクト.mp4",
-            "progress": 0,
-            "":
-            "status": "IN_PROGRESS",
-            "icon": "/userIcons/user01.png"
-        }
-"""
+                del (i)
+
+
 app = Flask(__name__)
 CORS(app)
 
 msg = sse_message()
 file_manage = file_manager()
 # SSE送信
+
+
 @app.route("/get_task")
 def sse():
     return Response(sse_make(), mimetype="text/event-stream")
+
 
 def sse_make():
     while True:
@@ -81,6 +83,7 @@ def sse_make():
         yield f"data: {res}\n\n"
         time.sleep(1)
 
+
 @app.route("/add_task", methods=["POST"])
 def add():
     file_manage.add_task(request.json)
@@ -88,6 +91,7 @@ def add():
     x = file_manage.get_task()
     print(x)
     return x
-if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0")
 
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
